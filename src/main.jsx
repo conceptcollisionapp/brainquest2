a8f127362606e7cddfd9c749eb3ca2ca9f704c6e
conceptcollisionapp <conceptcollisionapp@gmail.com>
2026-06-19 10:53:21 -0500

    Built with Nexus AI collaboration

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { ProgressProvider } from './context/ProgressContext.jsx';
+import './styles/global.css';
+import './styles/animations.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ProgressProvider>
+      <App />
+    </ProgressProvider>
+  </React.StrictMode>
+);
